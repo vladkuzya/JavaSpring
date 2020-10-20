ef37bab110767780bf66be4123df686852d9c683
Vlad Kuzmin <vladkuzmin22@gmail.com>
2020-10-20 21:30:35 +0300

    implemented Spring Web, UserController and UserResponceDto

--- src/main/java/mate/academy/dao/impl/UserDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/UserDaoImpl.java
@@ -47,4 +47,11 @@ public class UserDaoImpl implements UserDao {
             return session.createQuery("from User", User.class).getResultList();
         }
     }
+
+    @Override
+    public User getById(Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(User.class, id);
+        }
+    }
 }
