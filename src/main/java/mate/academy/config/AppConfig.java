f38169d6528fb9f3a3d8c29942d54da7ac65e588
Vlad Kuzmin <vladkuzmin22@gmail.com>
2020-10-20 11:43:45 +0300

    used a constructor instead annotation

--- src/main/java/mate/academy/config/AppConfig.java
+++ src/main/java/mate/academy/config/AppConfig.java
@@ -4,7 +4,6 @@ import java.util.Properties;
 import javax.sql.DataSource;
 import mate.academy.model.User;
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -19,8 +18,11 @@ import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
         "mate.academy.dao"
 })
 public class AppConfig {
-    @Autowired
-    private Environment environment;
+    private final Environment environment;
+
+    public AppConfig(Environment environment) {
+        this.environment = environment;
+    }
 
     @Bean
     public DataSource getDataSource() {
