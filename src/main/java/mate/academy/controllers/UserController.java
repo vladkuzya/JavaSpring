0f88a1215580dcd550fd9377fcf6dda963de3354
Vlad Kuzmin <vladkuzmin22@gmail.com>
2020-10-21 10:02:37 +0300

    took into account Sofia's comments

--- src/main/java/mate/academy/controllers/UserController.java
+++ src/main/java/mate/academy/controllers/UserController.java
@@ -8,20 +8,18 @@ import mate.academy.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
     @GetMapping("/inject")
-    @ResponseBody
     public String injectData() {
         userService.add(new User("bob@gmail.com", "b0b"));
         userService.add(new User("alica@gmail.com", "fox"));
